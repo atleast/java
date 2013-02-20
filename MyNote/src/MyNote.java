644f849470b9462f19f23fd559996c3c815e4569
atleast <atleast886@gmail.com>
2013-02-20 12:49:51 +0800

    modified mysql db and add some resource and CS

--- MyNote/src/MyNote.java
+++ MyNote/src/MyNote.java
@@ -22,7 +22,6 @@ public class MyNote extends JFrame implements ActionListener{
     /**
      * @param args
      */
-    //定义成员变量
     private JTextArea jTextArea = null;
     private JMenuBar jMenuBar = null;
     private JMenu menuFile = null;
@@ -37,12 +36,12 @@ public class MyNote extends JFrame implements ActionListener{
         MyNote myNote = new MyNote();
         
         String driver = "com.mysql.jdbc.Driver";
-        String url = "jdbc:mysql://localhost:3306/exercise";
+        String url = "jdbc:mysql://localhost:3306/mydb";
         String user = "root";
         String password = "";
         Connection ct = null;
         Statement stmt = null;
-        String sql = "insert into default_integer values(0,0,0,0,0,0)";
+        String sql = "insert into user values(1,'linda','f')";
         
         try {
 			Class.forName(driver);
@@ -65,7 +64,6 @@ public class MyNote extends JFrame implements ActionListener{
         
     }
     
-    //构造函数，初始化成员变量
     public MyNote(){
         
         jTextArea = new JTextArea();
@@ -83,28 +81,23 @@ public class MyNote extends JFrame implements ActionListener{
         menuFile.add( menuItemOpen );
         menuItemOpen.setText( "Open" );
         
-        //注册监听File->Open
         menuItemOpen.setActionCommand( "open" );
         menuItemOpen.addActionListener( this );
         
         menuItemSave = new JMenuItem();
         menuFile.add( menuItemSave );
         menuItemSave.setText( "Save" );
-        //注册监听File->Save
         menuItemSave.setActionCommand( "save" );
         menuItemSave.addActionListener( this );
         
         menuItemExit = new JMenuItem();
         menuItemExit.setText( "Exit" );
         menuFile.add( menuItemExit );
-        //注册监听File->Exit
+        
         menuItemExit.setActionCommand( "exit" );
         menuItemExit.addActionListener( this );
-        
-        //设置窗体属性
-        //窗体大小
+
         this.setSize( 500, 400 );
-        //窗体是否可以变化
         this.setResizable(false);
         this.setTitle("My Note");
         this.setLocation( 200, 200 );
@@ -115,15 +108,12 @@ public class MyNote extends JFrame implements ActionListener{
     @Override
     public void actionPerformed( ActionEvent e ) {
         
-        //监听File->Open功能
         if(e.getActionCommand().equals( "open" )){
-            //打开文件夹选择文件
             JFileChooser fileChooser = new JFileChooser();
             fileChooser.setDialogTitle( "Please select a file to open..." );
             fileChooser.showOpenDialog( null );
             
             String filePath = fileChooser.getSelectedFile().getAbsolutePath();
-            //定义文件输入流读取文件内容
             FileReader fr = null;
             BufferedReader br = null;
             
@@ -131,7 +121,6 @@ public class MyNote extends JFrame implements ActionListener{
                 fr = new FileReader(filePath);
                 br = new BufferedReader( fr );
                 
-                //从文件中读取内容
                 
                 String str = "";
                 String totalString = "";
@@ -140,7 +129,6 @@ public class MyNote extends JFrame implements ActionListener{
                     totalString += str + "\r\n";
                 }
                 
-                //将读取的内容显示到JTextArea
                 jTextArea.setText( totalString );
                 
             }
@@ -159,15 +147,12 @@ public class MyNote extends JFrame implements ActionListener{
             
         }
         
-        //监听File->Save
         if(e.getActionCommand().equals( "save" )){
-            //打开文件夹选择位置
             JFileChooser fileChooser = new JFileChooser();
             fileChooser.setDialogTitle( "Please select a folder..." );
             fileChooser.showSaveDialog( null );
             
             String filePath = fileChooser.getSelectedFile().getAbsolutePath();
-            //定义文件输出流写入文件内容
             FileWriter fw = null;
             BufferedWriter bw = null;
             
@@ -175,7 +160,6 @@ public class MyNote extends JFrame implements ActionListener{
                 fw = new FileWriter(filePath);
                 bw = new BufferedWriter(fw);
                 
-                //将文件内容写入文件
                 bw.write(this.jTextArea.getText());
             }
             catch ( IOException e1 ) {
@@ -191,8 +175,6 @@ public class MyNote extends JFrame implements ActionListener{
                 }
             }
         }
-        
-        //监听File->Exit
         if(e.getActionCommand().equals( "exit" )){
             System.exit(0);
         }
